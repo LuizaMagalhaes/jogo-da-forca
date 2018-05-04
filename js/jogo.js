e304d84fc6bd4a907865c536fd894fe4354cfd42
Luiza Magalhaes <luizalmagalhaes@hotmail.com>
2018-05-04 15:32:13 -0300

    implementando a logica do jogo

--- js/jogo.js
+++ js/jogo.js
@@ -25,10 +25,48 @@ function createGame(){
     return step;
   }
 
+  function processAttempt(attempt){
+    var exp = new RegExp("attempt", gi);
+    var result;
+    var right = false;
+
+    while (result = exp.exec(secretWord)) gaps[resul.index] = attempt;
+
+    if (!right) sprite.nextFrame();
+  }
+
+  function win(){
+    return gaps.length 
+    ? !gaps.some(function(gap) {
+        return gap == '';
+    })
+    : false;
+
+  }
+
+  function lose() {
+    return sprite.isFinished();
+  }
+
+  function winLose() {
+    return win() || lose();
+  }
+
+  function restart() {
+    step = 1;
+    gaps = [];
+    sprite.reset();
+  }
+
   return { 
     setSecretWord: setSecretWord,
     getGaps: getGaps,
-    getStep: getStep
+    getStep: getStep,
+    processAttempt: processAttempt,
+    win: win,
+    lose: lose,
+    winLose: winLose,
+    restart: restart
   }
 
 }
